2b172655e4c5d36d1fb4302d36233d12f857fe13
zrisher <zrisher@yahoo.com>
2017-05-15 06:19:23 -0700

    changes to unused sepl code

--- src/lib/sepl.py
+++ src/lib/sepl.py
@@ -2,17 +2,13 @@
 from subprocess import Popen, PIPE
 
 
-def run(exe_path, json_config_path, build_dir, version, publish=False):
-    command_parts = [
-        exe_path, json_config_path,
-        # 'version={}'.format(version), # parsing broken, now works from files
-        'publish={}'.format(publish)
-    ]
+def run_sepl(exe_path, json_config_path, build_dir, publish=False):
+    command_parts = [exe_path, json_config_path, 'publish={}'.format(publish)]
     print('Running SEPL command:', ' '.join(command_parts))
     proc = Popen(command_parts, stdout=PIPE, stderr=PIPE, cwd=build_dir)
     (stdout, stderr) = proc.communicate()
     if proc.returncode == 0:
-        print("SEPL succeeded: {}".format(stdout.decode('ascii')))
+        print("SEPL succeeded: " + stdout.decode('ascii'))
     else:
         raise ValueError('SEPL error.\nStdout:\n{}\nStderr:\n{}'
                          .format(stdout.decode('ascii'),
